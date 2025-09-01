c802b8f306a2b6abf2a088ed7b5c53b085a64d97
haibaaa <9700samarth@gmail.com>
2025-09-01 12:37:22 +0530

    mars

--- home/scripts/personal/mars.nix
+++ home/scripts/personal/mars.nix
@@ -1,22 +1,21 @@
 {pkgs, ...}: {
   home.packages = [
-    pkgs.writeShellScriptBin
-    "mars"
-    # bash
-    ''
-      # !/usr/bin/env bash
-      # Simple wrapper to run Mars MIPS from CLI
+    (pkgs.writeShellScriptBin "mars"
+      # bash
+      ''
+        # !/usr/bin/env bash
+        # Simple wrapper to run Mars MIPS from CLI
 
-      JAR="$HOME/apps/Mars.jar"
+        JAR="$HOME/apps/Mars.jar"
 
-      if [[ ! -f "$JAR" ]]; then
-          echo "Error: Mars.jar not found at $JAR"
-          exit 1
-      fi
+        if [[ ! -f "$JAR" ]]; then
+            echo "Error: Mars.jar not found at $JAR"
+            exit 1
+        fi
 
-      # Pass all arguments to Mars
-      java -jar "$JAR" "$@"
+        # Pass all arguments to Mars
+        java -jar "$JAR" "$@"
 
-    ''
+      '')
   ];
 }
